77374d03a7c7ef442cdde545e2ce47e2ea17e9e7
YS <yoonsam86@gmail.com>
2022-05-15 13:52:07 +0900

    PagerAdapter 프레그먼트 생성 추가

--- app/src/main/java/kr/co/fastcampus/co/kr/coroutines/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/kr/co/fastcampus/co/kr/coroutines/ui/main/SectionsPagerAdapter.kt
@@ -7,8 +7,17 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 class SectionsPagerAdapter(private val fragmentActivity: FragmentActivity) :
     FragmentStateAdapter(fragmentActivity) {
 
+    /**
+     * createFragment 는 상황에 따라 필요한것을 만들어내고 캐싱을 하게 되어 있습니다.
+     *
+     * ```
+     *  val imageSearchFragment = ImageSearchFragment()
+     * ```
+     * 이런식으로 변수를 만들어서 할당하게 되면 항상 만들어져 있기 때문에 곤란한 상황이 생깁니다.
+     * 생성과 소멸은 페이저 어댑터에게 맡깁니다.
+     */
     override fun createFragment(position: Int): Fragment {
-        TODO("프래그먼트를 생성해야합니다.")
+        return ImageSearchFragment()
     }
 
     override fun getItemCount(): Int {
